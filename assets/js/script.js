56c12b327e0bf1c8784dd7a9cf5b1e845c559c2a
RachelFurlong-dev <rachel.furlong@ekcgroup.ac.uk>
2023-04-16 15:38:37 +0000

    Add array of objects for the card images

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const cardArray = [
+{
+    name: 'robot 1', 
+    img: 'img/alien-insect-robot.png',
+},
+{
+    name: 'robot 2', 
+    img: 'img/alien-insect-robot2.png',
+},
+{
+    name: 'robot 3', 
+    img: 'img/blue-insect-robot.png',
+},
+{
+    name: 'robot 4', 
+    img: 'img/green-insect-robot.png',
+},
+{
+    name: 'robot 5', 
+    img: 'img/orange-insect-robot.png',
+},
+{
+    name: 'robot 6', 
+    img: 'img/pink-insect-robot.png',
+},
+{
+    name: 'robot 7', 
+    img: 'img/reddish-insect-robot.png',
+},
+{
+    name: 'robot 8', 
+    img: 'img/white-insect-robot.png',
+}
+];
